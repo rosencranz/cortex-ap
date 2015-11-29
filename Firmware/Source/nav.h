6be4ac8e646da76c140d973f74acaedfcee28dbe
rosencranz <rosenkranz@email.it>
2015-11-29 19:30:51 +0100

    function Nav_Wpt_Number() renamed Nav_Wpt_Number_Get() added function Nav_Wpt_Number_Set()

--- Firmware/Source/nav.h
+++ Firmware/Source/nav.h
@@ -9,7 +9,8 @@
  *
  * @file
  *
- * Change: corrected parameters of function Nav_Wpt_Set()
+ * Change: function Nav_Wpt_Number() renamed Nav_Wpt_Number_Get()
+ *         added function Nav_Wpt_Number_Set()
  *
  *============================================================================*/
 
@@ -33,17 +34,18 @@ typedef struct {    /* waypoint structure */
 
 /*---------------------------------- Interface -------------------------------*/
 
-void Nav_Load_Waypoints ( void );
-void Nav_Save_Home ( void );
-void Navigation ( void );
-float Nav_Altitude ( void );
-float Nav_Bearing_Rad ( void );
-float Nav_Alt_Error ( void );
-uint16_t Nav_Distance ( void );
-uint16_t Nav_Wpt_Number ( void );
-uint16_t Nav_Wpt_Index ( void );
-uint16_t Nav_Wpt_Altitude ( void );
-void Nav_Wpt_Get ( uint16_t index, STRUCT_WPT *wpt );
-void Nav_Wpt_Set ( uint16_t index, STRUCT_WPT *wpt );
+void     Nav_Load_Waypoints ( void );
+void     Nav_Save_Home      ( void );
+void     Navigation         ( void );
+float    Nav_Altitude       ( void );
+float    Nav_Bearing_Rad    ( void );
+float    Nav_Alt_Error      ( void );
+uint16_t Nav_Distance       ( void );
+uint16_t Nav_Wpt_Number_Get ( void );
+bool     Nav_Wpt_Number_Set ( uint16_t num );
+uint16_t Nav_Wpt_Index      ( void );
+uint16_t Nav_Wpt_Altitude   ( void );
+void     Nav_Wpt_Get        ( uint16_t index, STRUCT_WPT *wpt );
+void     Nav_Wpt_Set        ( uint16_t index, STRUCT_WPT *wpt );
 
 
