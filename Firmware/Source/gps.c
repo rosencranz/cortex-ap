625b3047fc9b7d9fcd7560e87e5012853636782b
rosencranz <rosenkranz@email.it>
2015-12-15 21:31:35 +0100

    added decoding of date and time from NMEA sentences

--- Firmware/Source/gps.c
+++ Firmware/Source/gps.c
@@ -4,7 +4,7 @@
  * @file gps.c
  * @brief GPS driver
  *
- * Change: function Parse_Gps(): read time from NMEA sentence
+ * Change: added decoding of date and time from NMEA sentences
  *
  *============================================================================*/
 
@@ -14,10 +14,11 @@
 #include "config.h"
 #include "gps.h"
 
+/*---------------------------------- Globals ---------------------------------*/
+
 /*--------------------------------- Definitions ------------------------------*/
 
 #define LINE_LENGTH 16      /* length of lines read */
-#define DATE_LENGTH 7       /* length of date string */
 
 /*----------------------------------- Macros ---------------------------------*/
 
@@ -32,14 +33,21 @@ typedef enum {
     NMEA_INVALID    /* invalid NMEA string */
 } ENUM_NMEA_TYPE;
 
+/* GPS time */
+typedef struct {
+	uint8_t hour;
+	uint8_t min;
+	uint8_t sec;
+	uint8_t day;
+	uint8_t mon;
+	uint8_t year;
+} STRUCT_GPS_TIME;
+
 /*---------------------------------- Constants -------------------------------*/
 
-/*---------------------------------- Globals ---------------------------------*/
-
-/*--------------------------------- Prototypes -------------------------------*/
-
-static void parse_coord( float * f_coord, uint8_t c );
-static bool cmp_prefix( const uint8_t * src , const uint8_t * dest );
+static const SerialConfig sdcfg = {
+  57600
+};
 
 /*----------------------------------- Locals ---------------------------------*/
 
@@ -49,7 +57,7 @@ static float f_curr_lat = 0.0f;             /* current latitude */
 static float f_curr_lon = 0.0f;             /* current longitude */
 
 static uint8_t sz_line[LINE_LENGTH];        /* input line */
-static uint8_t sz_date[LINE_LENGTH];        /* input line */
+static uint8_t sz_date[LINE_LENGTH];        /* date and time string */
 
 static uint32_t ul_temp_coord = 0UL;        /* temporary for coordinate parser */
 static uint16_t ui_gps_cog = 0;             /* aircraft course over ground [°] */
@@ -57,16 +65,18 @@ static uint16_t ui_gps_speed = 0;           /* speed [kt/10] */
 static uint16_t ui_gps_alt = 0;             /* altitude [m] */
 static uint8_t uc_gps_status = GPS_NOFIX;   /* status of GPS */
 static uint8_t uc_commas = 11;              /* counter of commas in NMEA sentence */
-static uint8_t prefix_index = 0;
-static uint8_t date_index = 0;
+static uint8_t uc_pref_index = 0;
+static uint8_t uc_date_index = 0;
 
 static ENUM_NMEA_TYPE e_nmea_type;
-
+static STRUCT_GPS_TIME gps_time;
 static Semaphore sem_gps;
 
-static const SerialConfig sdcfg = {
-  57600
-};
+/*--------------------------------- Prototypes -------------------------------*/
+
+static void parse_coord( float * f_coord, uint8_t c );
+static bool cmp_prefix( const uint8_t * src , const uint8_t * dest );
+static void set_time(uint8_t * str);
 
 /*---------------------------------- Functions -------------------------------*/
 
@@ -83,7 +93,7 @@ void GPS_Init ( void ) {
   /* Activates the Serial driver 2, PA2(TX) and PA3(RX) are routed to USART2. */
   palSetPadMode(GPIOA, 2, PAL_MODE_STM32_ALTERNATE_PUSHPULL);
   sdStart(&SD2, &sdcfg);
-  chSemInit(&sem_gps, 1);   /* Semaphore initialization */
+  chSemInit(&sem_gps, 1); 
 }
 
 /*----------------------------------------------------------------------------
@@ -173,133 +183,135 @@ void GPS_Parse( void ) {
     c = chnGetTimeout(&SD2, TIME_IMMEDIATE);
 
     if (c != Q_TIMEOUT) {
-//      chSemWait(&sem_gps);
-      if (c == '$') uc_commas = 0;              /* start of NMEA sentence */
-      if (c == ',') uc_commas++;                /* count commas */
+  //      chSemWait(&sem_gps);
+      if (c == '$') uc_commas = 0;            /* start of NMEA sentence */
+      if (c == ',') uc_commas++;              /* count commas */
 
       switch (uc_commas) {
         case 0:
-            if (prefix_index < 6) {
-                sz_line[prefix_index++] = c;    /* read prefix */
-            } else if (prefix_index == 6) {
-                sz_line[prefix_index++] = 0;    /* terminate prefix */
-            }
-            break;
+          if (uc_pref_index < 6) {
+            sz_line[uc_pref_index++] = c;      /* read prefix */
+          } else if (uc_pref_index == 6) {
+            sz_line[uc_pref_index++] = 0;      /* terminate prefix */
+          }
+          break;
 
-        case 1:                                 /* check prefix */
-            if (prefix_index != 0) {
-                if (cmp_prefix(sz_line, (const uint8_t *)"$GPRMC")) {
-                    e_nmea_type = NMEA_GPRMC;
-                } else if (cmp_prefix(sz_line, (const uint8_t *)"$GPGGA")) {
-                    e_nmea_type = NMEA_GPGGA;
-                } else {
-                    e_nmea_type = NMEA_INVALID;
-                    uc_commas = 11;
-                }
-                prefix_index = 0;
+        case 1:                               /* check prefix */
+          if (uc_pref_index != 0) {
+            if (cmp_prefix(sz_line, (const uint8_t *)"$GPRMC")) {
+              e_nmea_type = NMEA_GPRMC;
+            } else if (cmp_prefix(sz_line, (const uint8_t *)"$GPGGA")) {
+              e_nmea_type = NMEA_GPGGA;
             } else {
-                sz_date[date_index++] = c;      /* get time */
+              e_nmea_type = NMEA_INVALID;
+              uc_commas = 11;
             }
-            break;
+            uc_pref_index = 0;
+          } else {
+            sz_date[uc_date_index++] = c;        /* get time */
+          }
+          break;
 
         case 2:
-            if (e_nmea_type == NMEA_GPRMC) {    /* get fix info */
-                if (c == 'A') {
-                   uc_gps_status = GPS_FIX;
-                } else if (c == 'V') {
-                   uc_gps_status = GPS_NOFIX;
-                }
+          if (e_nmea_type == NMEA_GPRMC) {    /* get fix info */
+            if (c == 'A') {
+              uc_gps_status = GPS_FIX;
+            } else if (c == 'V') {
+              uc_gps_status = GPS_NOFIX;
             }
-            break;
+          }
+          break;
 
         case 3:
         case 4:
-            if (e_nmea_type == NMEA_GPRMC) {    /* get latitude data */
-                parse_coord (&f_temp_lat, c);
-            }
-            break;
+          if (e_nmea_type == NMEA_GPRMC) {    /* get latitude data */
+            parse_coord (&f_temp_lat, c);
+          }
+          break;
 
         case 5:
         case 6:
-            if (e_nmea_type == NMEA_GPRMC) {    /* get longitude data */
-                parse_coord (&f_temp_lon, c);
-            }
-            break;
+          if (e_nmea_type == NMEA_GPRMC) {    /* get longitude data */
+            parse_coord (&f_temp_lon, c);
+          }
+          break;
 
         case 7:
-            if (e_nmea_type == NMEA_GPRMC) {    /* get speed */
-                if (c == ',') {
-                    ui_gps_speed = 0;
-                } else if (c != '.') {
-                    ui_gps_speed *= 10;
-                    ui_gps_speed += (c - '0');
-                }
+          if (e_nmea_type == NMEA_GPRMC) {    /* get speed */
+            if (c == ',') {
+              ui_gps_speed = 0;
+            } else if (c != '.') {
+              ui_gps_speed *= 10;
+              ui_gps_speed += (c - '0');
             }
-            break;
+          }
+          break;
 
         case 8:
-            if (e_nmea_type == NMEA_GPRMC) {    /* get heading */
-                if (c == ',') {
-                    ui_gps_cog = 0;
-                } else if (c != '.') {
-                    ui_gps_cog *= 10;
-                    ui_gps_cog += (c - '0');
-                }
+          if (e_nmea_type == NMEA_GPRMC) {    /* get heading */
+            if (c == ',') {
+                ui_gps_cog = 0;
+            } else if (c != '.') {
+                ui_gps_cog *= 10;
+                ui_gps_cog += (c - '0');
             }
-            break;
+          }
+          break;
 
         case 9:
-            switch (e_nmea_type) {
-                case NMEA_GPRMC:                /* get date */
-                    if (c == ',') {
-                      date_index = 0;
-                    } else if (date_index < 6) {
-                      sz_date[date_index++] = c;  /* read date */
-                    } else {
-                      sz_date[date_index] = 0;  /* terminate date */
-                    }
-                    break;
+          switch (e_nmea_type) {
+            case NMEA_GPRMC:                /* get date */
+              if (c == ',') {
+                uc_date_index = 6;
+              } else if (uc_date_index < 12) {
+                sz_date[uc_date_index++] = c;  /* read date */
+              } else {
+                sz_date[uc_date_index] = 0;    /* terminate date */
+              }
+              break;
 
-                case NMEA_GPGGA:                /* get altitude */
-                    if (c == ',') {
-                      ui_gps_alt = 0;
-                    } else if (c != '.') {
-                      ui_gps_alt *= 10;
-                      ui_gps_alt += (c - '0');
-                    }
-                    break;
+            case NMEA_GPGGA:                /* get altitude */
+              if (c == ',') {
+                ui_gps_alt = 0;
+              } else if (c != '.') {
+                ui_gps_alt *= 10;
+                ui_gps_alt += (c - '0');
+              }
+              break;
 
-                case NMEA_INVALID:              /* invalid */
-                    break;
+            case NMEA_INVALID:              /* invalid */
+              break;
 
-                default:                        /* error */
-                    break;
-            }
-            break;
+            default:                        /* error */
+              break;
+          }
+          break;
 
         case 10:
-            if (uc_gps_status == GPS_FIX) {
-               switch (e_nmea_type) {
-                  case NMEA_GPRMC:              /* end of GPRMC sentence */
-                      ui_gps_cog /= 10;
-                      f_curr_lat = f_temp_lat;
-                      f_curr_lon = f_temp_lon;
-                      uc_commas = 11;
-                      break;
+          if (uc_gps_status == GPS_FIX) {
+            switch (e_nmea_type) {
+              case NMEA_GPRMC:              /* end of GPRMC sentence */
+                ui_gps_cog /= 10;
+                f_curr_lat = f_temp_lat;
+                f_curr_lon = f_temp_lon;
+                uc_commas = 11;
+                uc_date_index = 0;
+                set_time(sz_date);
+                break;
 
-                  case NMEA_GPGGA:              /* end of GPGGA sentence */
-                      ui_gps_alt /= 10;
-                      uc_commas = 11;
-                      break;
+              case NMEA_GPGGA:              /* end of GPGGA sentence */
+                ui_gps_alt /= 10;
+                uc_commas = 11;
+                break;
 
-                  default:                      /* error */
-                    break;
-               }
+              default:                      /* error */
+                break;
             }
-            break;
+          }
+          break;
 
         default:
-            break;
+          break;
       }                     /* end switch */
     }                       /* end if */
 //    chSemSignal(&sem_gps);
@@ -324,6 +336,30 @@ static bool cmp_prefix( const uint8_t * src , const uint8_t * dest ) {
         j++;
     }
     return b_match;
+}
+
+/*----------------------------------------------------------------------------
+ *
+ * @brief   Set GPS time
+ * @param   str = pointer to string containing date and time
+ * @returns -
+ * @remarks Expected format of string: hhmmssddmmyy
+ *
+ *---------------------------------------------------------------------------*/
+static void set_time( uint8_t * str ) {
+
+  gps_time.hour  = ((*str++) - '0') * 10;
+  gps_time.hour += ((*str++) - '0');
+  gps_time.min   = ((*str++) - '0') * 10;
+  gps_time.min  += ((*str++) - '0');
+  gps_time.sec   = ((*str++) - '0') * 10;
+  gps_time.sec  += ((*str++) - '0');
+  gps_time.day   = ((*str++) - '0') * 10;
+  gps_time.day  += ((*str++) - '0');      
+  gps_time.mon   = ((*str++) - '0') * 10;
+  gps_time.mon  += ((*str++) - '0') - 1;
+  gps_time.year  = ((*str++) - '0') * 10;
+  gps_time.year += ((*str++) - '0') + 100; 
 }
 
 /*----------------------------------------------------------------------------
